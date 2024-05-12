6a2e1fa84aa41df055320224435106234a43727b
DarkCaster <dark.caster@outlook.com>
2024-05-12 19:17:57 +0300

    logging: add ILogger interface, and console logger using go std log package

---
+++ logging/logging.go
@@ -0,0 +1,33 @@
+package logging
+
+type LogLevel int
+
+const (
+	ErrorLevel LogLevel = iota
+	WarnLevel
+	InfoLevel
+	DebugLevel
+	TraceLevel
+)
+
+type ILogger interface {
+	Tracef(format string, args ...any)
+	Debugf(format string, args ...any)
+	Infof(format string, args ...any)
+	Warnf(format string, args ...any)
+	Errorf(format string, args ...any)
+	Panicf(format string, args ...any)
+
+	Traceln(args ...any)
+	Debugln(args ...any)
+	Infoln(args ...any)
+	Warnln(args ...any)
+	Errorln(args ...any)
+	Panicln(args ...any)
+
+	SetLevel(newLevel LogLevel)
+}
+
+func NewSimpleLogger(initialLevel LogLevel) (ILogger, error) {
+	return newSimpleLogger(initialLevel)
+}
---
+++ logging/simple_logger.go
@@ -0,0 +1,91 @@
+package logging
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+//###NOUPLOAD###
+
+type SimpleLogger struct {
+	CurLevel     LogLevel
+	NormalLogger *log.Logger
+	ErrorLogger  *log.Logger
+}
+
+func newSimpleLogger(initialLevel LogLevel) (*SimpleLogger, error) {
+	return &SimpleLogger{CurLevel: initialLevel, NormalLogger: log.New(os.Stdout, "", log.Ltime), ErrorLogger: log.New(os.Stderr, "", log.Ltime)}, nil
+}
+
+func (l *SimpleLogger) Tracef(format string, args ...any) {
+	if l.CurLevel >= TraceLevel {
+		l.NormalLogger.Printf("[TRACE] "+format, args...)
+	}
+}
+
+func (l *SimpleLogger) Debugf(format string, args ...any) {
+	if l.CurLevel >= DebugLevel {
+		l.NormalLogger.Printf("[DEBUG] "+format, args...)
+	}
+}
+
+func (l *SimpleLogger) Infof(format string, args ...any) {
+	if l.CurLevel >= InfoLevel {
+		l.NormalLogger.Printf("[INFO] "+format, args...)
+	}
+}
+
+func (l *SimpleLogger) Warnf(format string, args ...any) {
+	if l.CurLevel >= WarnLevel {
+		l.NormalLogger.Printf("[WARN] "+format, args...)
+	}
+}
+
+func (l *SimpleLogger) Errorf(format string, args ...any) {
+	if l.CurLevel >= ErrorLevel {
+		l.ErrorLogger.Printf("[ERROR] "+format, args...)
+	}
+}
+
+func (l *SimpleLogger) Panicf(format string, args ...any) {
+	l.ErrorLogger.Panicf("[PANIC] "+format, args...)
+}
+
+func (l *SimpleLogger) Traceln(args ...any) {
+	if l.CurLevel >= TraceLevel {
+		l.NormalLogger.Println(fmt.Sprint("[TRACE]", fmt.Sprint(args...)))
+	}
+}
+
+func (l *SimpleLogger) Debugln(args ...any) {
+	if l.CurLevel >= DebugLevel {
+		l.NormalLogger.Println(fmt.Sprint("[DEBUG]", fmt.Sprint(args...)))
+	}
+}
+
+func (l *SimpleLogger) Infoln(args ...any) {
+	if l.CurLevel >= InfoLevel {
+		l.NormalLogger.Println(fmt.Sprint("[INFO]", fmt.Sprint(args...)))
+	}
+}
+
+func (l *SimpleLogger) Warnln(args ...any) {
+	if l.CurLevel >= WarnLevel {
+		l.NormalLogger.Println(fmt.Sprint("[WARN]", fmt.Sprint(args...)))
+	}
+}
+
+func (l *SimpleLogger) Errorln(args ...any) {
+	if l.CurLevel >= ErrorLevel {
+		l.ErrorLogger.Println(fmt.Sprint("[ERROR]", fmt.Sprint(args...)))
+	}
+}
+
+func (l *SimpleLogger) Panicln(args ...any) {
+	l.ErrorLogger.Panicln(fmt.Sprint("[PANIC]", fmt.Sprint(args...)))
+}
+
+func (l *SimpleLogger) SetLevel(newLevel LogLevel) {
+	l.CurLevel = newLevel
+}
