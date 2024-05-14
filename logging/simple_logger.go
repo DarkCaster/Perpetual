3ae93fe4c7b3b628e7921732747b620d6e890485
DarkCaster <dark.caster@outlook.com>
2024-05-14 04:14:23 +0300

    logging: SimpleLogger updated, show time since program start in sec.msec format

--- logging/simple_logger.go
+++ logging/simple_logger.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 //###NOUPLOAD###
@@ -15,81 +16,86 @@ type SimpleLogger struct {
 	CurLevel     LogLevel
 	NormalLogger *log.Logger
 	ErrorLogger  *log.Logger
+	Start        time.Time
 }
 
 func newSimpleLogger(initialLevel LogLevel) (*SimpleLogger, error) {
-	return &SimpleLogger{CurLevel: initialLevel, NormalLogger: log.New(os.Stdout, "", log.Ltime), ErrorLogger: log.New(os.Stderr, "", log.Ltime)}, nil
+	return &SimpleLogger{CurLevel: initialLevel, NormalLogger: log.New(os.Stdout, "", 0), ErrorLogger: log.New(os.Stderr, "", 0), Start: time.Now()}, nil
+}
+
+func (l *SimpleLogger) timer() string {
+	return fmt.Sprintf("[%06.3f] ", time.Since(l.Start).Seconds())
 }
 
 func (l *SimpleLogger) Tracef(format string, args ...any) {
 	if l.CurLevel >= TraceLevel {
-		l.NormalLogger.Print("[TRC] ", fmt.Sprintf(format, args...))
+		l.NormalLogger.Print(l.timer(), "[TRC] ", fmt.Sprintf(format, args...))
 	}
 }
 
 func (l *SimpleLogger) Debugf(format string, args ...any) {
 	if l.CurLevel >= DebugLevel {
-		l.NormalLogger.Print("[DBG] ", fmt.Sprintf(format, args...))
+		l.NormalLogger.Print(l.timer(), "[DBG] ", fmt.Sprintf(format, args...))
 	}
 }
 
 func (l *SimpleLogger) Infof(format string, args ...any) {
 	if l.CurLevel >= InfoLevel {
-		l.NormalLogger.Print("[INF] ", fmt.Sprintf(format, args...))
+		l.NormalLogger.Print(l.timer(), "[INF] ", fmt.Sprintf(format, args...))
 	}
 }
 
 func (l *SimpleLogger) Warnf(format string, args ...any) {
 	if l.CurLevel >= WarnLevel {
-		l.NormalLogger.Print("[WRN] ", fmt.Sprintf(format, args...))
+		l.NormalLogger.Print(l.timer(), "[WRN] ", fmt.Sprintf(format, args...))
 	}
 }
 
 func (l *SimpleLogger) Errorf(format string, args ...any) {
 	if l.CurLevel >= ErrorLevel {
-		l.ErrorLogger.Print("[ERR] ", fmt.Sprintf(format, args...))
+		l.ErrorLogger.Print(l.timer(), "[ERR] ", fmt.Sprintf(format, args...))
 	}
 }
 
 func (l *SimpleLogger) Panicf(format string, args ...any) {
 	msg := fmt.Sprintf(format, args...)
-	l.ErrorLogger.Print("[PNC] ", msg)
+	l.ErrorLogger.Print(l.timer(), "[PNC] ", msg)
 	panic(msg)
 }
 
 func (l *SimpleLogger) Traceln(args ...any) {
 	if l.CurLevel >= TraceLevel {
-		l.NormalLogger.Print("[TRC] ", fmt.Sprintln(args...))
+		l.NormalLogger.Print(l.timer(), "[TRC] ", fmt.Sprintln(args...))
 	}
 }
 
 func (l *SimpleLogger) Debugln(args ...any) {
 	if l.CurLevel >= DebugLevel {
-		l.NormalLogger.Print("[DBG] ", fmt.Sprintln(args...))
+		l.NormalLogger.Print(l.timer(), "[DBG] ", fmt.Sprintln(args...))
 	}
 }
 
 func (l *SimpleLogger) Infoln(args ...any) {
 	if l.CurLevel >= InfoLevel {
-		l.NormalLogger.Print("[INF] ", fmt.Sprintln(args...))
+		l.NormalLogger.Print(l.timer(), "[INF] ", fmt.Sprintln(args...))
 	}
 }
 
 func (l *SimpleLogger) Warnln(args ...any) {
 	if l.CurLevel >= WarnLevel {
-		l.NormalLogger.Print("[WRN] ", fmt.Sprintln(args...))
+		l.NormalLogger.Print(l.timer(), "[WRN] ", fmt.Sprintln(args...))
 	}
 }
 
 func (l *SimpleLogger) Errorln(args ...any) {
 	if l.CurLevel >= ErrorLevel {
-		l.ErrorLogger.Print("[ERR] ", fmt.Sprintln(args...))
+		l.ErrorLogger.Print(l.timer(), "[ERR] ", fmt.Sprintln(args...))
 	}
 }
 
 func (l *SimpleLogger) Panicln(args ...any) {
 	msg := fmt.Sprintln(args...)
-	l.ErrorLogger.Print("[PNC] ", msg)
+	l.ErrorLogger.Print(l.timer(), "[PNC] ", msg)
 	panic(msg)
 }
 
