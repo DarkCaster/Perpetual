15d9d56991351942c044f6f56d5e0e71844c5105
DarkCaster <dark.caster@outlook.com>
2024-08-05 16:47:07 +0300

    prompts updated, moving identical prompt-texts into separate file

--- prompts/dotnetfw_prompts.go
+++ prompts/dotnetfw_prompts.go
@@ -43,7 +43,7 @@ func (p *DotNetFWPrompts) GetImplementStage2FilesToChangePrompt() string {
 }
 
 func (p *DotNetFWPrompts) GetImplementStage2FilesToChangeExtendedPrompt() string {
-	return "Here are the contents of the source code files that interest me. The files contain sections of code that need to be implemented. They are marked with the comment \"###IMPLEMENT###\". Review all the source code provided to you and create a list of file names that will be changed or created by you as a result of implementing the code. Place each filename in <filename></filename> tags.\n\nAfter the list of file names, write your reasoning about what needs to be done in these files to implement the task. Don't write actual code in your reasoning yet. Place reasoning in a single block between tags <reasoning></reasoning>"
+	return DefaultImplementStage2FilesToChangeExtendedPrompt
 }
 
 func (p *DotNetFWPrompts) GetImplementStage2NoPlanningPrompt() string {
