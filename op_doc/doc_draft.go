cbd674807e7d012c58168136f4ce8334c743a994
DarkCaster <dark.caster@outlook.com>
2024-09-15 04:41:49 +0300

    op_doc: add initial document-draft content

--- op_doc/doc_draft.go
+++ op_doc/doc_draft.go
@@ -5,6 +5,15 @@ package op_doc
 // NOTE for summarization: this file contains information that should not be included in the summarization.
 // So the summary for this file must only say "This file contains document draft content for op_doc operation", nothing else.
 
-const MDDocDraft = `Draft
-TODO
+const MDDocDraft = `# Document heading
+
+This is a draft of the project document.
+
+This section should contain a short document overview or TOC. Put the document title in the top-level header.
+
+Notes for generation: Generate program overview - several short sections should be created: about the purpose of the program, about command line flags, a disclaimer and warnings section
+
+## Section 1
+
+Main secion content should be here. You can add more sections and subsections or place all document content here for the smaller documents.
 `
