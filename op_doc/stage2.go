90100c6ba18b9a20afb53aa16d6a7d9804da5893
DarkCaster <dark.caster@outlook.com>
2024-09-16 18:51:21 +0300

    op_doc: stage2 refactor

--- op_doc/stage2.go
+++ op_doc/stage2.go
@@ -46,48 +46,48 @@ func Stage2(projectRootDir string, perpetualDir string, promptsDir string, syste
 	logger.Debugln("Created project-index request message")
 
 	// Create project-index simulated response
-	stage2ProjectIndexResponseMessage := llm.AddPlainTextFragment(llm.NewMessage(llm.SimulatedAIResponse), loadPrompt(prompts.AIDocProjectIndexResponseFile))
-	messages = append(messages, stage2ProjectIndexResponseMessage)
+	projectIndexResponseMessage := llm.AddPlainTextFragment(llm.NewMessage(llm.SimulatedAIResponse), loadPrompt(prompts.AIDocProjectIndexResponseFile))
+	messages = append(messages, projectIndexResponseMessage)
 	logger.Debugln("Created project-index simulated response message")
 
 	// Add files requested by LLM
 	if len(filesForReview) > 0 {
 		// Create request with file-contents
-		stage2ProjectSourceCodeMessage := llm.AddPlainTextFragment(llm.NewMessage(llm.UserRequest), loadPrompt(prompts.DocProjectCodePromptFile))
+		sourceCodeMessage := llm.AddPlainTextFragment(llm.NewMessage(llm.UserRequest), loadPrompt(prompts.DocProjectCodePromptFile))
 		for _, item := range filesForReview {
 			contents, err := utils.LoadTextFile(filepath.Join(projectRootDir, item))
 			if err != nil {
 				logger.Panicln("Failed to add file contents to stage2 prompt", err)
 			}
-			stage2ProjectSourceCodeMessage = llm.AddFileFragment(stage2ProjectSourceCodeMessage, item, contents, fileNameTags)
+			sourceCodeMessage = llm.AddFileFragment(sourceCodeMessage, item, contents, fileNameTags)
 		}
-		messages = append(messages, stage2ProjectSourceCodeMessage)
+		messages = append(messages, sourceCodeMessage)
 		logger.Debugln("Project source code message created")
 
 		// Create simulated response
-		stage2ProjectSourceCodeResponseMessage := llm.AddPlainTextFragment(llm.NewMessage(llm.SimulatedAIResponse), loadPrompt(prompts.AIDocProjectCodeResponseFile))
-		messages = append(messages, stage2ProjectSourceCodeResponseMessage)
+		sourceCodeResponseMessage := llm.AddPlainTextFragment(llm.NewMessage(llm.SimulatedAIResponse), loadPrompt(prompts.AIDocProjectCodeResponseFile))
+		messages = append(messages, sourceCodeResponseMessage)
 		logger.Debugln("Project source code simulated response added")
 	} else {
 		logger.Infoln("Not creating extra source-code review")
 	}
 
 	// Create document-processing request message
-	stage2PromptFile := prompts.DocStage2WritePromptFile
+	docPromptFile := prompts.DocStage2WritePromptFile
 	if action == "REFINE" {
-		stage2PromptFile = prompts.DocStage2RefinePromptFile
+		docPromptFile = prompts.DocStage2RefinePromptFile
 	}
 
-	stage2DocProcessRequestMessage := llm.AddPlainTextFragment(llm.NewMessage(llm.UserRequest), loadPrompt(stage2PromptFile))
+	docRequestMessage := llm.AddPlainTextFragment(llm.NewMessage(llm.UserRequest), loadPrompt(docPromptFile))
 	contents, err := utils.LoadTextFile(filepath.Join(projectRootDir, targetDocument))
 	if err != nil {
 		logger.Panicln("failed to add document contents to stage1 prompt", err)
 	}
-	stage2DocProcessRequestMessage = llm.AddPlainTextFragment(stage2DocProcessRequestMessage, contents)
-	messages = append(messages, stage2DocProcessRequestMessage)
-	logger.Debugln("Created target files analysis request message")
+	docRequestMessage = llm.AddPlainTextFragment(docRequestMessage, contents)
+	messages = append(messages, docRequestMessage)
+	logger.Debugln("Created document processing request message")
 
-	stage2ContinuePrompt := loadPrompt(prompts.DocStage2ContinuePromptFile)
+	continuePrompt := loadPrompt(prompts.DocStage2ContinuePromptFile)
 
 	//Make LLM request, process response
 	onFailRetriesLeft := connector.GetOnFailureRetryLimit()
@@ -96,7 +96,7 @@ func Stage2(projectRootDir string, perpetualDir string, promptsDir string, syste
 	}
 
 	for ; onFailRetriesLeft >= 0; onFailRetriesLeft-- {
-		// Create a copy, so it will be automatically discarded on file retry
+		// Work with a copy of message-chain, to discard it on retry
 		messagesTry := append([]llm.Message(nil), messages...)
 		// Initialize temporary variables for handling partial answers
 		var responses []string
@@ -128,7 +128,7 @@ func Stage2(projectRootDir string, perpetualDir string, promptsDir string, syste
 				}
 				// Add partial response to stage2 messages, with request to continue
 				messagesTry = append(messagesTry, llm.SetRawResponse(llm.NewMessage(llm.SimulatedAIResponse), aiResponse))
-				messagesTry = append(messagesTry, llm.AddPlainTextFragment(llm.NewMessage(llm.UserRequest), stage2ContinuePrompt))
+				messagesTry = append(messagesTry, llm.AddPlainTextFragment(llm.NewMessage(llm.UserRequest), continuePrompt))
 			}
 			// Append response fragment
 			responses = append(responses, aiResponse)
@@ -136,7 +136,7 @@ func Stage2(projectRootDir string, perpetualDir string, promptsDir string, syste
 		if fileRetry {
 			continue
 		}
-		// Join responses together to the final document contents
+		// Join responses together to form the final document contents
 		return strings.Join(responses, "")
 	}
 	return ""
