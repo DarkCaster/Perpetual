045ddf64d6f2e11e213d1c40561010dd5fc70d46
DarkCaster <dark.caster@outlook.com>
2024-11-07 01:30:52 +0300

    llm: mitmHTTPClient fixes, inject values properly

--- llm/mitmHTTPClient.go
+++ llm/mitmHTTPClient.go
@@ -2,24 +2,25 @@ package llm
 
 import (
 	"bytes"
+	"encoding/json"
 	"io"
 	"net/http"
 
 	"github.com/DarkCaster/Perpetual/utils"
 )
 
-func NewMitmHTTPClient(jsonToInject string) *http.Client {
-	return &http.Client{ //nolint:gochecknoglobals
+func NewMitmHTTPClient(valuesToInject map[string]interface{}) *http.Client {
+	return &http.Client{
 		Transport: &mitmTransport{
-			Transport:    http.DefaultTransport,
-			JsonToInject: jsonToInject,
+			Transport:      http.DefaultTransport,
+			ValuesToInject: valuesToInject,
 		},
 	}
 }
 
 type mitmTransport struct {
-	Transport    http.RoundTripper
-	JsonToInject string
+	Transport      http.RoundTripper
+	ValuesToInject map[string]interface{}
 }
 
 func (t *mitmTransport) RoundTrip(req *http.Request) (*http.Response, error) {
@@ -43,9 +44,22 @@ func (t *mitmTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	} else {
 		return t.Transport.RoundTrip(req)
 	}
-	//TODO: (not now) Combine injected JSON with body
-	newBody := /*t.JsonToInject + */ string(bodyData)
+	bodyObj := map[string]interface{}{}
+	if err := json.Unmarshal(bodyData, &bodyObj); err != nil {
+		return nil, err
+	}
+	// inject new values to body json at the top level
+	for name, value := range t.ValuesToInject {
+		bodyObj[name] = value
+	}
+	// convert modified body back into JSON
+	newBody, err := json.Marshal(bodyObj)
+	if err != nil {
+		return nil, err
+	}
 	// Create new ReadCloser with modified body
-	req.Body = io.NopCloser(bytes.NewReader([]byte(newBody)))
+	req.Body = io.NopCloser(bytes.NewReader(newBody))
+	req.ContentLength = int64(len(newBody))
+	// Perform actual http request with new body
 	return t.Transport.RoundTrip(req)
 }
