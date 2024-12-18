7227b0eb2f6f3ef520e9799e76c9bd78d56e43ad
DarkCaster <dark.caster@outlook.com>
2024-12-18 05:35:31 +0300

    prompts: remove unused filenames consts

--- prompts/prompt_filenames.go
+++ prompts/prompt_filenames.go
@@ -9,58 +9,11 @@ const ProjectFilesWhitelistFileName = "project_files_whitelist.json"
 const ProjectFilesToMarkdownLangMappingFileName = "project_files_to_markdown_lang_mappings.json"
 const ProjectFilesBlacklistFileName = "project_files_blacklist.json"
 const ProjectTestFilesBlacklistFileName = "project_test_files_blacklist.json"
-const FileNameTagsRXFileName = "filename_tags_regexps.json"
-const NoUploadCommentRXFileName = "no_upload_comment_regexps.json"
-const OpImplementCommentRXFileName = "op_implement_comment_regexps.json"
-const FileNameEmbedRXFileName = "filename_embed_regexp.json"
-const OutputTagsRXFileName = "output_tags_regexps.json"
-const ReasoningsTagsRXFileName = "reasonings_tags_regexps.json"
-const ReasoningsTagsFileName = "reasonings_tags.json"
 
 // System prompts
 const SystemPromptsConfigFile = "system_prompts.json"
 
-// Annotate-operation config
+// Names for config files for different operations
 const OpAnnotateConfigFile = "op_annotate.json"
-
-// Annotate-operation config
 const OpImplementConfigFile = "op_implement.json"
-
-// Implement-operation stage1 prompt-filenames
-const ImplementStage1ProjectIndexPromptFile = "op_implement_stage1_project_index_prompt.txt"
-const AIImplementStage1ProjectIndexResponseFile = "op_implement_stage1_project_index_ai_response.txt"
-const ImplementStage1SourceAnalysisPromptFile = "op_implement_stage1_source_analysis_prompt.txt"
-
-// Implement-operation stage2 prompt-filenames
-const ImplementStage2ProjectCodePromptFile = "op_implement_stage2_project_code_prompt.txt"
-const AIImplementStage2ProjectCodeResponseFile = "op_implement_stage2_project_code_ai_response.txt"
-const ImplementStage2FilesToChangePromptFile = "op_implement_stage2_files_to_change_prompt.txt"
-const ImplementStage2FilesToChangeExtendedPromptFile = "op_implement_stage2_files_to_change_extended_prompt.txt"
-
-const ImplementStage2NoPlanningPromptFile = "op_implement_stage2_no_planning_prompt.txt"
-const AIImplementStage2NoPlanningResponseFile = "op_implement_stage2_no_planning_ai_response.txt"
-
-// Implement-operation stage3 prompt-filenames
-const ImplementStage3ChangesDonePromptFile = "op_implement_stage3_changes_done_prompt.txt"
-const AIImplementStage3ChangesDoneResponseFile = "op_implement_stage3_changes_done_ai_response.txt"
-const ImplementStage3ProcessFilePromptFile = "op_implement_stage3_process_file_prompt.txt"
-const ImplementStage3ContinuePromptFile = "op_implement_stage3_continue_prompt.txt"
-
-// Doc-operation prompt-filenames
-const DocProjectIndexPromptFile = "op_doc_project_index_prompt.txt"
-const AIDocProjectIndexResponseFile = "op_doc_project_index_ai_response.txt"
-const DocProjectCodePromptFile = "op_doc_project_code_prompt.txt"
-const AIDocProjectCodeResponseFile = "op_doc_project_code_ai_response.txt"
-const DocExamplePromptFile = "op_doc_example_prompt.txt"
-const AIDocExampleResponseFile = "op_doc_example_ai_response.txt"
-
-// Doc-operation write-action prompt-filenames
-const DocStage1WritePromptFile = "op_doc_stage1_write_prompt.txt"
-const DocStage2WritePromptFile = "op_doc_stage2_write_prompt.txt"
-
-// Doc-operation refine-action prompt-filenames
-const DocStage1RefinePromptFile = "op_doc_stage1_refine_prompt.txt"
-const DocStage2RefinePromptFile = "op_doc_stage2_refine_prompt.txt"
-const DocStage2ContinuePromptFile = "op_doc_stage2_continue_prompt.txt"
-
-const PromptsDir = "prompts"
+const OpDocConfigFile = "op_doc.json"
