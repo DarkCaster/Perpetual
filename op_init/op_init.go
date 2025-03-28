d71ac87a4934144f7d6cbfd145ba50f9138ef30a
DarkCaster <dark.caster@outlook.com>
2025-03-29 02:56:37 +0300

    op_init: generated .gitignore for .perpetual dir updated - ignore .message_log.txt* files

--- op_init/op_init.go
+++ op_init/op_init.go
@@ -97,7 +97,7 @@ func Run(version string, args []string, logger logging.ILogger) {
 	// Create a .gitignore file in the .perpetual directory
 	logger.Traceln("Creating .gitignore file")
 
-	gitignoreText := fmt.Sprintf("/%s\n/%s\n/%s\n/%s\n", utils.DotEnvFileName, utils.AnnotationsFileName, llm.LLMRawLogFile, utils.StashesDirName)
+	gitignoreText := fmt.Sprintf("/%s\n/%s\n/%s*\n/%s\n", utils.DotEnvFileName, utils.AnnotationsFileName, llm.LLMRawLogFile, utils.StashesDirName)
 	err = utils.SaveTextFile(filepath.Join(perpetualDir, ".gitignore"), gitignoreText)
 	if err != nil {
 		logger.Panicln("Error creating .gitignore file:", err)
