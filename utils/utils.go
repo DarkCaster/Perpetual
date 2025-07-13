fdba813a4b524624805b9ede190d9ca707d8dc6c
DarkCaster <dark.caster@outlook.com>
2025-07-13 05:24:48 +0300

    utils: improve perpetual dir and project dir detection logic

--- utils/utils.go
+++ utils/utils.go
@@ -21,15 +21,31 @@ func FindProjectRoot(logger logging.ILogger) (string, string, error) {
 	if err != nil {
 		return "", "", err
 	}
-	projectRootDir, perpetualDir, err := findProjectRoot(cwd, logger)
-	// Check if projectRootDir is a symbolic link
+	projectRootDir := ""
+	perpetualDir := ""
+	// Try to get perpetualDir from ENV, or use default discovery logic
+	if envDir, errEnv := GetEnvString("PERPETUAL_DIR"); errEnv == nil {
+		projectRootDir = cwd
+		perpetualDir = envDir
+	} else {
+		projectRootDir, perpetualDir, err = findProjectRoot(cwd, logger)
+	}
 	if err == nil {
+		// Check presence of perpetualDir directory
+		info, err := os.Stat(perpetualDir)
+		if err != nil {
+			return projectRootDir, perpetualDir, err
+		}
+		if !info.IsDir() {
+			return projectRootDir, perpetualDir, fmt.Errorf(".perpetual directory is not a directory: %s", perpetualDir)
+		}
+		// Check that projectRootDir directory is not a symlink
 		fileInfo, err := os.Lstat(projectRootDir)
 		if err != nil {
 			return projectRootDir, perpetualDir, err
 		}
 		if fileInfo.Mode()&os.ModeSymlink != 0 {
-			return projectRootDir, perpetualDir, fmt.Errorf("dir is a symlink or reparse point: %s", projectRootDir)
+			return projectRootDir, perpetualDir, fmt.Errorf("project directory is a symlink or reparse point: %s", projectRootDir)
 		}
 	}
 	return projectRootDir, perpetualDir, err
