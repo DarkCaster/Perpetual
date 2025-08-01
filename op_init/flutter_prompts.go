a559403500098cbd17bee032d502c298a38679bb
DarkCaster <dark.caster@outlook.com>
2025-08-02 01:05:40 +0300

    op_init, flutterPrompts: fixes, add project prompts

--- op_init/flutter_prompts.go
+++ op_init/flutter_prompts.go
@@ -23,16 +23,13 @@ func (p *flutterPrompts) GetAnnotateConfig() map[string]interface{} {
 		{"(?i)^.*\\.l10n\\.yaml$", defaultAIAnnotatePrompt_Flutter_l10n_YAML, defaultAIAnnotatePrompt_Flutter_l10n_YAML_Short},
 		{"(?i)^.*\\.pubspec\\.yaml$", defaultAIAnnotatePrompt_Flutter_Pubspec_YAML, defaultAIAnnotatePrompt_Flutter_Pubspec_YAML_Short},
 		// C, C++ files for native windows or linux builds
-		//TODO: blacklist linux/flutter/ephemeral/
 		{"(?i)^.*(CMakeLists.txt|\\.cmake)", defaultAIAnnotatePrompt_Cmake, defaultAIAnnotatePrompt_Cmake_Short},
 		{"(?i)^.*\\.(c|cc)$", defaultAIAnnotatePrompt_C, defaultAIAnnotatePrompt_C_Short},
 		{"(?i)^.*\\.(cpp|cxx|c\\+\\+|cppm)$", defaultAIAnnotatePrompt_CPP, defaultAIAnnotatePrompt_CPP_Short},
 		{"(?i)^.*\\.(h|h\\+\\+|hpp|hh|tpp|ipp)$", defaultAIAnnotatePrompt_H_CPP, defaultAIAnnotatePrompt_H_CPP_Short},
-		{"(?i)^.*\\.(s|asm)$", defaultAIAnnotatePrompt_S, defaultAIAnnotatePrompt_S_Short},
 		{"(?i)^.*\\.rc$", defaultAIAnnotatePrompt_CPP_Windows_RC, defaultAIAnnotatePrompt_CPP_Windows_RC_Short},
 		{"(?i)^.*\\.exe\\.manifest$", defaultAIAnnotatePrompt_EXE_Manifest_Windows, defaultAIAnnotatePrompt_EXE_Manifest_Windows_short},
 		// files for android build
-		//TODO: blacklist android/app/src/main/java/io/flutter/plugins
 		{"(?i)^.*\\.java$", defaultAIAnnotatePrompt_Flutter_Java, defaultAIAnnotatePrompt_Flutter_Java_Short},
 		{"(?i)^.*\\.kt$", defaultAIAnnotatePrompt_Flutter_Kotlin, defaultAIAnnotatePrompt_Flutter_Kotlin_Short},
 		//TODO: source files for mac, ios and web builds support
@@ -45,7 +42,7 @@ func (p *flutterPrompts) GetImplementConfig() map[string]interface{} {
 	result := getDefaultImplementConfigTemplate()
 	result[config.K_SystemPrompt] = "You are a highly skilled Flutter/Dart software developer. When you write code, you output the entire file with your changes without truncating it."
 	// redefine language-dependent prompt
-	result[config.K_ProjectIndexPrompt] = "Here is a description of the project in the Go programming language. Brief descriptions of the project source code files are provided, indicating the path to the file and the entities it contains."
+	result[config.K_ProjectIndexPrompt] = "Here is a description of the project in the Dart programming language and Flutter SDK. Brief descriptions of the project source code files are provided, indicating the path to the file and the entities it contains."
 	result[config.K_ImplementCommentsRx] = []string{"^\\s*\\/\\/\\s*###IMPLEMENT###.*$"}
 	result[config.K_NoUploadCommentsRx] = []string{"^\\s*\\/\\/\\s*###NOUPLOAD###.*$"}
 	return result
@@ -55,7 +52,7 @@ func (p *flutterPrompts) GetDocConfig() map[string]interface{} {
 	result := getDefaultDocConfigTemplate()
 	result[config.K_SystemPrompt] = "You are a highly skilled Flutter/Dart software developer. You write and refine technical documentation based on detailed study of the source code."
 	// redefine language-dependent prompt
-	result[config.K_ProjectIndexPrompt] = "Here is a description of the project in the Go programming language. Brief descriptions of the project source code files are provided, indicating the path to the file and the entities it contains."
+	result[config.K_ProjectIndexPrompt] = "Here is a description of the project in the Dart programming language and Flutter SDK. Brief descriptions of the project source code files are provided, indicating the path to the file and the entities it contains."
 	result[config.K_NoUploadCommentsRx] = []string{"^\\s*\\/\\/\\s*###NOUPLOAD###.*$"}
 	return result
 }
@@ -64,19 +61,44 @@ func (p *flutterPrompts) GetExplainConfig() map[string]interface{} {
 	result := getDefaultExplainConfigTemplate()
 	result[config.K_SystemPrompt] = "You are a highly skilled Flutter/Dart software developer. You are an expert in studying source code and finding solutions to software development questions. Your answers are detailed and consistent."
 	// redefine language-dependent prompt
-	result[config.K_ProjectIndexPrompt] = "Here is a description of the project in the Go programming language. Brief descriptions of the project source code files are provided, indicating the path to the file and the entities it contains."
+	result[config.K_ProjectIndexPrompt] = "Here is a description of the project in the Dart programming language and Flutter SDK. Brief descriptions of the project source code files are provided, indicating the path to the file and the entities it contains."
 	result[config.K_NoUploadCommentsRx] = []string{"^\\s*\\/\\/\\s*###NOUPLOAD###.*$"}
 	return result
 }
 
 func (p *flutterPrompts) GetProjectConfig() map[string]interface{} {
 	result := getDefaultProjectConfigTemplate()
-	result[config.K_ProjectFilesWhitelist] = []string{"(?i)^.*\\.go$"}
-	result[config.K_ProjectFilesBlacklist] = []string{"(?i)^vendor(\\\\|\\/).*"}
+	result[config.K_ProjectFilesWhitelist] = []string{
+		// dart files
+		"(?i)^.*\\.dart$",
+		"(?i)^.*\\.arb$",
+		"(?i)^.*\\.l10n\\.yaml$",
+		"(?i)^.*\\.pubspec\\.yaml$",
+		// c,c++ files for windows and linux builds
+		"(?i)^.*(CMakeLists.txt|\\.cmake)",
+		"(?i)^.*\\.(c|cc)$",
+		"(?i)^.*\\.(cpp|cxx|c\\+\\+|cppm)$",
+		"(?i)^.*\\.(h|h\\+\\+|hpp|hh|tpp|ipp)$",
+		"(?i)^.*\\.rc$",
+		"(?i)^.*\\.exe\\.manifest$",
+		// java and kotlin sources for android builds
+		"(?i)^.*\\.java$",
+		"(?i)^.*\\.kt$",
+		//TODO: source files for mac, ios and web builds support
+	}
+	result[config.K_ProjectFilesBlacklist] = []string{
+		//linux, windows and macos builds autogenerated files
+		"(?i)^.*(\\\\|\\/)(linux|windows|ios|macos)(\\\\|\\/)flutter(\\\\|\\/).*",
+		//autogenerated source files
+		"(?i)^.*(\\\\|\\/)io(\\\\|\\/)flutter(\\\\|\\/)plugins(\\\\|\\/).*",
+		"(?i)^.*(\\\\|\\/)localization(\\\\|\\/)app_localizations\\.dart$",
+		"(?i)^.*(\\\\|\\/)localization(\\\\|\\/)app_localizations_.*\\.dart$",
+		//build dir
+		"(?i)^build(\\\\|\\/).*",
+	}
 	result[config.K_ProjectTestFilesBlacklist] = []string{
-		"(?i)^.*_test\\.go$",
-		"(?i)^.*(\\\\|\\/)test(\\\\|\\/).*\\.go$",
-		"(?i)^test(\\\\|\\/).*\\.go$",
+		"(?i)^.*(\\\\|\\/)test(\\\\|\\/).*\\.dart$",
+		"(?i)^test(\\\\|\\/).*\\.dart$",
 	}
 	return result
 }
