ad251f8ed006f6f6df40c6ed93de97f3e61036e7
DarkCaster <dark.caster@outlook.com>
2025-10-20 06:37:36 +0300

    config: (work in progress) add PathDataCollection interface/struct for use with logic that select correct annotation prompt based project file-name match

--- config/config.go
+++ config/config.go
@@ -4,6 +4,17 @@ import (
 	"regexp"
 )
 
+// File path matcher holding some path-related data that can be used if path matched
+type PathData[T any] interface {
+	Match(path string) bool
+	GetData() []T
+}
+
+// Container with PathData instances in same order as in config file, return associated data for the first patch match (if any)
+type PathDataCollection[T any] interface {
+	GetDataIfMatch(path string) (bool, []T)
+}
+
 type Config interface {
 	Float(key string) float64
 	Integer(key string) int
@@ -13,4 +24,6 @@ type Config interface {
 	StringArray(key string) []string
 	StringArray2D(key string) [][]string
 	RegexpArray(key string) []*regexp.Regexp
+	PathWithStringData(key string) PathDataCollection[string]
+	PathWithIntegerData(key string) PathDataCollection[int]
 }
--- config/config_utils.go
+++ config/config_utils.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"reflect"
 	"regexp"
 )
 
@@ -137,4 +138,79 @@ func interfaceTo2DStringArray(source interface{}) [][]string {
 		target[i] = interfaceToStringArray(element)
 	}
 	return target
+}
+
+type rxDataPair[T any] struct {
+	Rx   *regexp.Regexp
+	Data []T
+}
+
+func (p *rxDataPair[T]) Match(path string) bool {
+	return p.Rx.MatchString(path)
+}
+
+func (p *rxDataPair[T]) GetData() []T {
+	return p.Data
+}
+
+type rxDataCollection[T any] struct {
+	Pairs []*rxDataPair[T]
+}
+
+func (c *rxDataCollection[T]) GetDataIfMatch(path string) (bool, []T) {
+	for _, p := range c.Pairs {
+		if p.Match(path) {
+			return true, p.Data
+		}
+	}
+	return false, nil
+}
+
+func newRxDataPair[T any](rxStr string, opts []any) (*rxDataPair[T], error) {
+	if len(opts) == 0 {
+		return nil, fmt.Errorf("no data provided along with path-regexp: %s", rxStr)
+	}
+	data := make([]T, len(opts))
+	for i := 0; i < len(opts); i++ {
+		if val, ok := opts[i].(T); ok {
+			data[i] = val
+		} else {
+			typeName := reflect.TypeFor[T]().String()
+			return nil, fmt.Errorf("array element at position %d is not a valid value of type %s", i+2, typeName)
+		}
+	}
+	if rx, err := regexp.Compile(rxStr); err == nil {
+		return &rxDataPair[T]{Rx: rx, Data: data}, nil
+	} else {
+		return nil, fmt.Errorf("failed to compile path-regexp: %s: %v", rxStr, err)
+	}
+}
+
+// source must be 2-d array from json, inner array first element is a regex string, next elements is a path options data, and it must be assignable to T type
+func newRxDataCollection[T any](optsCount int, source []any) (PathDataCollection[T], error) {
+	if optsCount < 1 {
+		return nil, fmt.Errorf("options count must be > 0")
+	}
+	collection := make([]*rxDataPair[T], len(source))
+	for i, el := range source {
+		if innerArr, ok := el.([]any); ok {
+			if len(innerArr) != optsCount+1 {
+				typeName := reflect.TypeFor[T]().String()
+				return nil, fmt.Errorf("inner array at pos %d must contain path-regexp followed by exactly %d parameters of type %s", i+1, optsCount, typeName)
+			}
+			if rxStr, ok := innerArr[0].(string); ok {
+				if pair, err := newRxDataPair[T](rxStr, innerArr[1:]); err == nil {
+					collection[i] = pair
+				} else {
+					return nil, fmt.Errorf("inner array at pos %d processing failed: %v", i+1, err)
+				}
+			} else {
+				return nil, fmt.Errorf("inner array at pos %d first element is not a string path-regexp", i+1)
+			}
+		} else {
+			typeName := reflect.TypeFor[T]().String()
+			return nil, fmt.Errorf("inner array at pos %d is not a valid array of path-regexp with corresponding options of type %s", i+1, typeName)
+		}
+	}
+	return &rxDataCollection[T]{Pairs: collection}, nil
 }
