1ddc8ca6b7c9328587089d49213aaeb78562a119
DarkCaster <dark.caster@outlook.com>
2026-07-25 05:13:25 +0300

    op_init: split internal init logic to the separate file 'init.go', preparing to merge 'init' and 'misc' operations into new single 'project' operation

---
+++ op_init/init.go
@@ -0,0 +1,132 @@
+package op_init
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/DarkCaster/Perpetual/config"
+	"github.com/DarkCaster/Perpetual/llm"
+	"github.com/DarkCaster/Perpetual/logging"
+	"github.com/DarkCaster/Perpetual/op_implement"
+	"github.com/DarkCaster/Perpetual/utils"
+)
+
+func Init(version, lang string, envExamples bool, logger logging.ILogger) error {
+	if lang == "" {
+		return errors.New("language must be provided")
+	}
+
+	logger.Debugln("Parsed language:", lang)
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		logger.Panicln("Error getting current working directory:", err)
+	}
+
+	perpetualDir := ""
+	if envDir, errEnv := utils.GetEnvString("PERPETUAL_DIR"); errEnv == nil {
+		perpetualDir = envDir
+	} else {
+		perpetualDir = filepath.Join(cwd, ".perpetual")
+	}
+
+	info, err := os.Stat(perpetualDir)
+	if err == nil {
+		if !info.IsDir() {
+			logger.Panicln(".perpetual already exists and it is not a directory!")
+		}
+		logger.Warnln("Directory .perpetual already exists, replacing configs:", perpetualDir)
+	} else if !os.IsNotExist(err) {
+		logger.Panicln("Error checking for .perpetual directory:", err)
+	} else {
+		logger.Infoln("Creating .perpetual directory:", perpetualDir)
+		err = os.Mkdir(perpetualDir, 0755)
+		if err != nil {
+			logger.Panicln("Error creating .perpetual directory:", err)
+		}
+	}
+
+	const DotEnvMaskName = "*.env"
+
+	// Create a .gitignore file in the .perpetual directory
+	logger.Traceln("Creating .gitignore file")
+
+	gitignoreText := fmt.Sprintf("/%s\n/%s\n/%s\n/%s\n/%s*\n/%s\n/%s\n", DotEnvMaskName, utils.AnnotationsFileName, utils.EmbeddingsFileName, utils.LockFileName, llm.LLMRawLogFile, utils.StashesDirName, op_implement.StateFileName)
+	_, err = utils.SaveTextFile(filepath.Join(perpetualDir, ".gitignore"), gitignoreText)
+	if err != nil {
+		logger.Panicln("Error creating .gitignore file:", err)
+	}
+
+	if envExamples {
+		logger.Traceln("Creating env example files")
+
+		for _, example := range GetEnvExampleCatalogWithVersion(version) {
+			if _, err = utils.SaveTextFile(filepath.Join(perpetualDir, example.Filename), example.Content); err != nil {
+				logger.Panicln("Error creating env example file:", err)
+			}
+		}
+	}
+
+	logger.Tracef("Creating %s file", descriptionTemplateFileName)
+	if _, err = utils.SaveTextFile(filepath.Join(perpetualDir, descriptionTemplateFileName), descriptionTemplate); err != nil {
+		logger.Panicf("Error creating %s file: %v", descriptionTemplateFileName, err)
+	}
+
+	// Create a prompt-files based on the selected language
+	logger.Debugln("Creating prompt-files")
+	promptsObj, err := newPrompts(lang)
+	if err != nil {
+		return err
+	}
+
+	saveConfig := func(filePath string, v any) {
+		logger.Traceln("Saving config:", filePath)
+		err = utils.SaveJsonFile(filepath.Join(perpetualDir, filePath), v)
+		if err != nil {
+			logger.Panicln(err)
+		}
+	}
+
+	// Save operation-config files
+	saveConfig(config.OpAnnotateConfigFile, promptsObj.GetAnnotateConfig())
+	saveConfig(config.OpImplementConfigFile, promptsObj.GetImplementConfig())
+	saveConfig(config.OpDocConfigFile, promptsObj.GetDocConfig())
+	saveConfig(config.OpReportConfigFile, promptsObj.GetReportConfig())
+	saveConfig(config.OpExplainConfigFile, promptsObj.GetExplainConfig())
+
+	// Save project-config file
+	saveConfig(config.ProjectConfigFile, promptsObj.GetProjectConfig())
+
+	obsoleteFiles := []string{
+		"filename_embed_regexp.json",
+		"filename_tags_regexps.json",
+		"filename_tags.json",
+		"no_upload_comment_regexps.json",
+		"output_tags_regexps.json",
+		"project_files_blacklist.json",
+		"project_files_to_markdown_lang_mappings.json",
+		"project_files_whitelist.json",
+		"reasonings_tags_regexps.json",
+		"reasonings_tags.json",
+	}
+
+	for _, file := range obsoleteFiles {
+		filePath := filepath.Join(perpetualDir, file)
+		if _, err := os.Stat(filePath); err == nil {
+			logger.Warnln("Obsolete config file found (remove it manually):", file)
+		}
+	}
+
+	obsoleteDirs := []string{"prompts"}
+
+	for _, dir := range obsoleteDirs {
+		dirPath := filepath.Join(perpetualDir, dir)
+		if _, err := os.Stat(dirPath); err == nil {
+			logger.Warnln("Obsolete directory found (remove it manually):", dir)
+		}
+	}
+
+	return nil
+}
--- op_init/op_init.go
+++ op_init/op_init.go
@@ -2,16 +2,9 @@ package op_init
 
 import (
 	"flag"
-	"fmt"
-	"os"
-	"path/filepath"
 
-	"github.com/DarkCaster/Perpetual/config"
-	"github.com/DarkCaster/Perpetual/llm"
 	"github.com/DarkCaster/Perpetual/logging"
-	"github.com/DarkCaster/Perpetual/op_implement"
 	"github.com/DarkCaster/Perpetual/usage"
-	"github.com/DarkCaster/Perpetual/utils"
 )
 
 const OpName = "init"
@@ -22,15 +15,17 @@ func initFlags() *flag.FlagSet {
 }
 
 func Run(version string, args []string, logger logging.ILogger) {
-	lang := ""
+	var lang, mode string
 	var help, verbose, trace, envExamples bool
+
 	// Parse flags for the "init" operation
 	initFlags := initFlags()
+	initFlags.StringVar(&mode, "m", "", "Select operation mode: init, ...")
 	initFlags.BoolVar(&help, "h", false, "Show usage")
-	initFlags.StringVar(&lang, "l", "", "Select programming language for setting up default LLM prompts (valid values: go|dotnet|bash|python3|vb6|c|cpp|arduino|flutter)")
+	initFlags.StringVar(&lang, "l", "", "Select programming language for '-m init' to setup project configuration with default LLM prompts (valid values: go|dotnet|bash|python3|vb6|c|cpp|arduino|flutter)")
+	initFlags.BoolVar(&envExamples, "ex", false, "Create env-file examples inside .perpetual dir, for use with '-m init'")
 	initFlags.BoolVar(&verbose, "v", false, "Enable debug logging")
 	initFlags.BoolVar(&trace, "vv", false, "Enable debug and trace logging")
-	initFlags.BoolVar(&envExamples, "ex", false, "Create env-file examples inside .perpetual dir")
 	initFlags.Parse(args)
 
 	if verbose {
@@ -48,117 +43,18 @@ func Run(version string, args []string, logger logging.ILogger) {
 		usage.PrintOperationUsage("", initFlags)
 	}
 
-	if lang == "" {
-		usage.PrintOperationUsage("language must be provided", initFlags)
+	var err error = nil
+	switch mode {
+	case "init":
+		err = Init(version, lang, envExamples, logger)
+	case "":
+		usage.PrintOperationUsage("You must provide a valid operation mode with the '-m' flag", initFlags)
+	default:
+		logger.Errorln("Invalid operation mode:", mode)
+		usage.PrintOperationUsage("You must provide a valid operation mode with the '-m' flag", initFlags)
 	}
 
-	logger.Debugln("Parsed language:", lang)
-
-	cwd, err := os.Getwd()
-	if err != nil {
-		logger.Panicln("Error getting current working directory:", err)
-	}
-
-	perpetualDir := ""
-	if envDir, errEnv := utils.GetEnvString("PERPETUAL_DIR"); errEnv == nil {
-		perpetualDir = envDir
-	} else {
-		perpetualDir = filepath.Join(cwd, ".perpetual")
-	}
-
-	info, err := os.Stat(perpetualDir)
-	if err == nil {
-		if !info.IsDir() {
-			logger.Panicln(".perpetual already exists and it is not a directory!")
-		}
-		logger.Warnln("Directory .perpetual already exists, replacing configs:", perpetualDir)
-	} else if !os.IsNotExist(err) {
-		logger.Panicln("Error checking for .perpetual directory:", err)
-	} else {
-		logger.Infoln("Creating .perpetual directory:", perpetualDir)
-		err = os.Mkdir(perpetualDir, 0755)
-		if err != nil {
-			logger.Panicln("Error creating .perpetual directory:", err)
-		}
-	}
-
-	const DotEnvMaskName = "*.env"
-
-	// Create a .gitignore file in the .perpetual directory
-	logger.Traceln("Creating .gitignore file")
-
-	gitignoreText := fmt.Sprintf("/%s\n/%s\n/%s\n/%s\n/%s*\n/%s\n/%s\n", DotEnvMaskName, utils.AnnotationsFileName, utils.EmbeddingsFileName, utils.LockFileName, llm.LLMRawLogFile, utils.StashesDirName, op_implement.StateFileName)
-	_, err = utils.SaveTextFile(filepath.Join(perpetualDir, ".gitignore"), gitignoreText)
-	if err != nil {
-		logger.Panicln("Error creating .gitignore file:", err)
-	}
-
-	if envExamples {
-		logger.Traceln("Creating env example files")
-
-		for _, example := range GetEnvExampleCatalogWithVersion(version) {
-			if _, err = utils.SaveTextFile(filepath.Join(perpetualDir, example.Filename), example.Content); err != nil {
-				logger.Panicln("Error creating env example file:", err)
-			}
-		}
-	}
-
-	logger.Tracef("Creating %s file", descriptionTemplateFileName)
-	if _, err = utils.SaveTextFile(filepath.Join(perpetualDir, descriptionTemplateFileName), descriptionTemplate); err != nil {
-		logger.Panicf("Error creating %s file: %v", descriptionTemplateFileName, err)
-	}
-
-	// Create a prompt-files based on the selected language
-	logger.Debugln("Creating prompt-files")
-	promptsObj, err := newPrompts(lang)
 	if err != nil {
 		usage.PrintOperationUsage(err.Error(), initFlags)
-	}
-
-	saveConfig := func(filePath string, v any) {
-		logger.Traceln("Saving config:", filePath)
-		err = utils.SaveJsonFile(filepath.Join(perpetualDir, filePath), v)
-		if err != nil {
-			logger.Panicln(err)
-		}
-	}
-
-	// Save operation-config files
-	saveConfig(config.OpAnnotateConfigFile, promptsObj.GetAnnotateConfig())
-	saveConfig(config.OpImplementConfigFile, promptsObj.GetImplementConfig())
-	saveConfig(config.OpDocConfigFile, promptsObj.GetDocConfig())
-	saveConfig(config.OpReportConfigFile, promptsObj.GetReportConfig())
-	saveConfig(config.OpExplainConfigFile, promptsObj.GetExplainConfig())
-
-	// Save project-config file
-	saveConfig(config.ProjectConfigFile, promptsObj.GetProjectConfig())
-
-	obsoleteFiles := []string{
-		"filename_embed_regexp.json",
-		"filename_tags_regexps.json",
-		"filename_tags.json",
-		"no_upload_comment_regexps.json",
-		"output_tags_regexps.json",
-		"project_files_blacklist.json",
-		"project_files_to_markdown_lang_mappings.json",
-		"project_files_whitelist.json",
-		"reasonings_tags_regexps.json",
-		"reasonings_tags.json",
-	}
-
-	for _, file := range obsoleteFiles {
-		filePath := filepath.Join(perpetualDir, file)
-		if _, err := os.Stat(filePath); err == nil {
-			logger.Warnln("Obsolete config file found (remove it manually):", file)
-		}
-	}
-
-	obsoleteDirs := []string{"prompts"}
-
-	for _, dir := range obsoleteDirs {
-		dirPath := filepath.Join(perpetualDir, dir)
-		if _, err := os.Stat(dirPath); err == nil {
-			logger.Warnln("Obsolete directory found (remove it manually):", dir)
-		}
 	}
 }
